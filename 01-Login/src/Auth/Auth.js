ecd89e2a0a7a19bd49d0c87bf969292cee094bf3
Umar Muneer <umarmuneer@Educative7s-MacBook-Pro.local>
2018-04-09 11:47:55 +0500

    using env variables

--- 01-Login/src/Auth/Auth.js
+++ 01-Login/src/Auth/Auth.js
@@ -1,13 +1,12 @@
 import history from '../history';
 import auth0 from 'auth0-js';
-import { AUTH_CONFIG } from './auth0-variables';
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
-    domain: AUTH_CONFIG.domain,
-    clientID: AUTH_CONFIG.clientId,
-    redirectUri: AUTH_CONFIG.callbackUrl,
-    audience: `https://${AUTH_CONFIG.domain}/userinfo`,
+    domain: process.env.REACT_APP_AUTH0_DOMAIN,
+    clientID: process.env.REACT_APP_AUTH0_CLIENTID,
+    redirectUri: process.env.REACT_APP_AUTH0_REDIRECT_URI,
+    audience: process.env.REACT_APP_AUTH0_AUDIENCE,
     responseType: 'token id_token',
     scope: 'openid'
   });
--- 01-Login/src/index.js
+++ 01-Login/src/index.js
@@ -2,9 +2,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { makeMainRoutes } from './routes';
-
+import dotenv from 'dotenv';
 const routes = makeMainRoutes();
-
+dotenv.config();
 ReactDOM.render(
   routes,
   document.getElementById('root')
